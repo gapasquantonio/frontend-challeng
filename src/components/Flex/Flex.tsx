bc7d6c26a09a1950176a186bced2eabaf68d397e
gapasquantonio <pasquantonioo@gmail.com>
2023-12-13 10:47:20 -0300

    feat: add Flex

---
+++ src/components/Flex/Flex.tsx
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+// defines the gap prop types
+interface GapProps<
+  ThemeType extends Theme = RequiredTheme,
+  TVal = ThemeValue<"space", ThemeType>
+> {
+  gap?: ResponsiveValue<TVal, ThemeType>;
+  rowGap?: ResponsiveValue<TVal, ThemeType>;
+  columnGap?: ResponsiveValue<TVal, ThemeType>;
+}
+
+// defaults when a value isn't used
+const defaults = { space: theme.space };
+import {
+  RequiredTheme,
+  ResponsiveValue,
+  system,
+  Theme,
+  ThemeValue,
+} from "styled-system";
+import theme, { StyledSystemProps, StyledSystemStyles } from "../../theme";
+
+// append the gap properties to flex box with the default for styled-system
+const Flex = styled.div<StyledSystemProps & GapProps>(
+  { display: "flex" },
+  StyledSystemStyles,
+  system({
+    gap: { property: "gap", scale: "space", defaultScale: defaults.space },
+    rowGap: {
+      property: "rowGap",
+      scale: "space",
+      defaultScale: defaults.space,
+    },
+    columnGap: {
+      property: "columnGap",
+      scale: "space",
+      defaultScale: defaults.space,
+    },
+  })
+);
+
+export default React.memo(Flex);
