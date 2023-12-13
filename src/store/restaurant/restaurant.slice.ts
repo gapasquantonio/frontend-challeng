d646230a501cdaea539b3316164fce5d2765cdbe
gapasquantonio <pasquantonioo@gmail.com>
2023-12-13 11:17:55 -0300

    feat: add mocked data

--- src/store/restaurant/restaurant.slice.ts
+++ src/store/restaurant/restaurant.slice.ts
@@ -1,44 +1,15 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-import { Restaurant, WebSettings } from "../../models/Restaurant";
+import { Restaurant } from "../../models/Restaurant";
 import restaurantService from "../../services/restaurant.service";
+import MockedRestaurant from "../../mocks/restaurant/MockedRestaurant";
 
 export interface RestaurantState {
   selectedRestaurantDetails?: Restaurant;
 }
 
 const initialState: RestaurantState = {
-  selectedRestaurantDetails: {
-    id: 7602,
-    name: "BURGERS RESTAURANT",
-    internalName: "BURGERS RESTAURANT",
-    description: null,
-    liveFlag: 1,
-    demoFlag: 1,
-    address1: "Rua XX-X, 1-11",
-    address2: "XXX",
-    address3: null,
-    city: "Bauru",
-    county: "BR",
-    postcode: "17012-360",
-    country: "BR",
-    timezoneOffset: "-03:00",
-    locale: "pt-BR",
-    timeZone: "America/Sao_Paulo",
-    webSettings: {
-      id: 5854,
-      venueId: 7602,
-      bannerImage:
-        "https://preodemo.gumlet.io/usr/venue/7602/web/646fbf3abf9d0.png",
-      backgroundColour: "#ffffff",
-      primaryColour: "#4f372f",
-      primaryColourHover: "#4f372f",
-      navBackgroundColour: "#4f372f",
-    },
-    ccy: "BRL",
-    ccySymbol: "R$",
-    currency: "R$",
-  },
+  selectedRestaurantDetails: MockedRestaurant,
 };
 
 const relatorioSlice = createSlice({
