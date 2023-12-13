bf17165bfd5dfa6a51b855df83e18e546b76403d
gapasquantonio <pasquantonioo@gmail.com>
2023-12-13 10:26:00 -0300

    feat: create device slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import deviceSlice from "./device/device.slice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    device: deviceSlice,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
