c1764b146b7dfdafdafb94e575b137fbd0431e06
gapasquantonio <pasquantonioo@gmail.com>
2023-12-13 23:38:16 -0300

    feat: remove unused cvars

--- src/features/ItemDetail/ItemDetail.tsx
+++ src/features/ItemDetail/ItemDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles, { RadioButton } from "./ItemDetail.styles";
 import { css } from "@emotion/react";
 import { Item, ModifierItem } from "../../models/MenuDetails";
@@ -54,9 +54,6 @@ function ItemDetailComponent({
     setTotalAmount(item.price);
     setCheckedOption(item);
   };
-  const cart = useMemo(() => {
-    return cartDetails && cartDetails?.cart;
-  }, [checkedOption, cartDetails?.cart]);
 
   const handleIncrease = () => {
     setItemQuantiyCounter((prev) => prev + 1);
@@ -68,9 +65,7 @@ function ItemDetailComponent({
   useEffect(() => {
     setTotalAmount(basePrice * ItemQuantityCounter);
   }, [ItemQuantityCounter]);
-  const [isLoading, setIsLoading] = useState(false);
   const handleAddItemToCart = () => {
-    setIsLoading(true);
     const isItemAleadyAddedToCart = checkIfItemAlreadyWasAddedInsideCart(
       id,
       cartDetails?.cart as Cart[]
@@ -84,8 +79,8 @@ function ItemDetailComponent({
         },
         checkedOption
       );
-      dispatch(setAddItemToCart(payload));
-      setIsLoading(false);
+      dispatch(setAddItemToCart(payload as Cart));
+
       return onClose();
     }
     const updatePayload = preparePayloadItemToBeUpdated(
